2c8494f4b98f8a039d27a0148958c9e7e29a9339
Luis Colorado <lc@luiscoloradosistemas.com>
2015-12-22 18:50:34 +0200

    Set the license terms.

--- HowToMakeAPostRequest/src/my/domain/http/client/MainHTTPClient.java
+++ HowToMakeAPostRequest/src/my/domain/http/client/MainHTTPClient.java
@@ -4,17 +4,19 @@
  * Project: HowToMakeAPostRequest
  * Filename: MainHTTPClient.java
  * Disclaimer: (C) 2015 LUIS COLORADO SISTEMAS S.L.
- * 		All rights reserved.  This file must considered
- *		confidential.
+ * 		All rights reserved.  This program is Open Source, you
+ * can distribute and derive work from it, under the coverage of 
+ * the BSD license.
  */
 package my.domain.http.client;
 
-import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
+ * Class to test a normal http connection to send a post request to the
+ * server.
+ * 
  * @author Luis Colorado <code>&lt;lc@luiscoloradosistemas.com&gt;</code>}
  *
  */
@@ -24,24 +26,30 @@ public class MainHTTPClient {
         try {
             // This is the URL we are going to connect to.
             URL url = new URL("http://www.google.com/");
+   
             // This is the data (json text) we are going to send.
             // We need to know it in advance to calculate the
             // Content-length header.
             String content="{'field_a': 'Arriba Cachipurriana'}\n";
+         
             // Get the connection to the server.
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            
             // Set the method name.
             conn.setRequestMethod("POST");
+            
             // content headers to send a fixed length text.
             conn.setRequestProperty("content-type", "text/json");
             conn.setRequestProperty("content-length", "" + content.length());
+            
             conn.connect();
+            
             int resp_code = conn.getResponseCode();
             String resp_string = conn.getResponseMessage();
+            
             System.out.println("" + resp_code + " " + resp_string);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
