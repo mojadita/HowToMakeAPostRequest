18b47c7199ecdc63d4c8832f8532e2f63f5dcff7
Luis Colorado <lc@luiscoloradosistemas.com>
2015-12-22 17:39:10 +0200

    new package and MainHTTPClient class

---
+++ HowToMakeAPostRequest/src/my/domain/http/client/MainHTTPClient.java
@@ -0,0 +1,24 @@
+/* $Id$
+ * Author: Luis Colorado <lc@luiscoloradosistemas.com>
+ * Date: 22 de dic. de 2015
+ * Project: HowToMakeAPostRequest
+ * Filename: MainHTTPClient.java
+ * Disclaimer: (C) 2015 LUIS COLORADO SISTEMAS S.L.
+ * 		All rights reserved.  This file must considered
+ *		confidential.
+ */
+package my.domain.http.client;
+
+
+/**
+ * @author Luis Colorado <code>&lt;lc@luiscoloradosistemas.com&gt;</code>}
+ *
+ */
+public class MainHTTPClient {
+
+    public static void main( String[] args ) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ HowToMakeAPostRequest/src/my/domain/http/client/package-info.java
@@ -0,0 +1,14 @@
+/* $Id$
+ * Author: Luis Colorado <lc@luiscoloradosistemas.com>
+ * Date: 22 de dic. de 2015
+ * Project: HowToMakeAPostRequest
+ * Filename: package-info.java
+ * Disclaimer: (C) 2015 LUIS COLORADO SISTEMAS S.L.
+ * 		All rights reserved.  This file must considered
+ *		confidential.
+ */
+/**
+ * @author Luis Colorado <code>&lt;lc@luiscoloradosistemas.com&gt;</code>}
+ *
+ */
+package my.domain.http.client;
